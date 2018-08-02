0970f83401981440f508d0d2a655adbae7d564a4
shalu scaria <shalu.scaria3@gmail.com>
2018-08-01 23:00:40 -0700

    calculate btn on keyboard

--- WindowShopper/Controller/MainVC.swift
+++ WindowShopper/Controller/MainVC.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet var wageText: CustomPriceCheckerTF!
+    @IBOutlet var priceText: CustomPriceCheckerTF!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        calcBtn.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageText.inputAccessoryView = calcBtn
+        priceText.inputAccessoryView = calcBtn
     }
 
-    
+    @objc func calculate(){
+        
+    }
 
 
 }
