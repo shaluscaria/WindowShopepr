9d64602f50c4b208aae390db178ebbc6da590aff
shalu scaria <shalu.scaria3@gmail.com>
2018-08-08 21:36:16 -0700

    completed

--- WindowShopper/View/CustomPriceCheckerTF.swift
+++ WindowShopper/View/CustomPriceCheckerTF.swift
@@ -12,6 +12,22 @@ import UIKit
 
 @IBDesignable class CustomPriceCheckerTF: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 20
+        let currencyLbl = UILabel(frame:(CGRect(x: 5, y: frame.size.height/2-size/2, width: size, height: size)))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.5723067522, green: 0.5723067522, blue: 0.5723067522, alpha: 0.4284300086)
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.textAlignment = .center
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
         customizeTextField()
@@ -26,6 +42,7 @@ import UIKit
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
